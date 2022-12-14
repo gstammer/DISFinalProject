03d4a76c988b82098b9508643ad1ab97e63bd195
gstammer <cprebels333@comcast.net>
2022-12-14 20:05:16 +0100

    updated food

--- Assets/Scripts/FoodScript.cs
+++ Assets/Scripts/FoodScript.cs
@@ -6,18 +6,30 @@ public class FoodScript : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject foodBar;
+    public float bobbingHeight = 0.5f;
+    public float bobbingRate = 0.05f;
+
+    private Vector3 startPos;
+    private Vector3 topPos;
+    private Vector3 currDestination;
     
     void Start()
     {
         //need to add section to where the object moves up and down;
+        startPos = transform.position;
+        topPos = startPos;
+        topPos.y += bobbingHeight;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (transform.position == startPos) currDestination = topPos;
+        if (transform.position == topPos) currDestination = startPos;
 
-        
+        transform.position = Vector3.MoveTowards(transform.position, currDestination, bobbingRate);
     }
+
     private void OnCollisionEnter(Collision collision)
     {
         Debug.Log("Burger collided with something " + collision.gameObject);
@@ -25,8 +37,6 @@ public class FoodScript : MonoBehaviour
             Debug.Log("Fund the player");
             foodBar.GetComponent<FoodBar>().increaseByFood();
             Destroy(gameObject);
-            
-
         }
     }
 }
