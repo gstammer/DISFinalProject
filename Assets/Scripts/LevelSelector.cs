6f959aae4dc38f359dcd4e38864db4d5f48fb923
gstammer <cprebels333@comcast.net>
2022-12-16 20:42:15 +0100

    fixing up ui

--- Assets/Scripts/LevelSelector.cs
+++ Assets/Scripts/LevelSelector.cs
@@ -10,9 +10,9 @@ public class LevelSelector : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.None;
     }
-    
+
     // Update is called once per frame
     public void OpenScene()
     {
